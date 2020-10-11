deafde7c08a0c3ce89515f2793a238a139a133cf
Admir Alicic <admir@Admirs-iMac.local>
2020-10-11 10:26:17 +0200

    Add to cart functionality

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
-import { get } from 'mongoose';
 import { CART_ADD_ITEM } from '../constants/cartConstants';
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
-  const { data } = await axios.get(`/api/products/$${id}`);
+  const { data } = await axios.get(`/api/products/${id}`);
 
   dispatch({
     type: CART_ADD_ITEM,
